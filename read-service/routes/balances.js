99914c7a7fadb2dd536280d4c7aa84d9296f2f6b
Chan <chan.suttichujit@exxonmobil.com>
2019-08-16 23:10:36 +0700

    concat multiple event pages

--- read-service/routes/balances.js
+++ read-service/routes/balances.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET users listing. */
-router.get('/:userId/balances', function (req, res, next) {
+router.get('/:userId/balances', async function (req, res, next) {
     var userId = req.params.userId;
     let requestDateTime = new Date(req.query.dateTime)
     var getProjectionUrl = 'http://34.87.47.20:2113/projection/user_{userId}_projection/state';
@@ -12,44 +13,50 @@ router.get('/:userId/balances', function (req, res, next) {
 
     console.log(getProjectionUrl);
     if(requestDateTime != "Invalid Date") {
-      const options = {
-        url: getAllEventUrl,
-        headers: {
-          'Accept': 'application/vnd.eventstore.atom+json'
-        }
+
+      var headers = {
+        'Accept': 'application/vnd.eventstore.atom+json'
       };
-      request(options,
-          function (error, response, body) {
-            console.log("start calculate")
-            var events = JSON.parse(body).entries
-            if (!error && response.statusCode == 200) {
-              var sum = 0;
-              var size = events.length;
-              console.log(size)
 
-              var start = Date.now()
-              for(var i = size-1 ; i >=0 ; i--){
-                let date = new Date(events[i].updated)
-                if(date < requestDateTime) {
-                  const eventData = JSON.parse(events[i].data)
-                  sum += eventData.amount
-                }
-                else {
-                  break;
-                }
+      var totalEvents = []
+      while(getAllEventUrl != undefined){
 
-              }
-              var end = Date.now()
-              console.log(end - start)
-              res.send({
-                totalBalance: sum
-              });
-            } else {
-              res.send('user not found');
-            }
-          })
+        console.log(getAllEventUrl)
+        console.log("start collecting events")
+
+        let body = await axios.get(getAllEventUrl, { headers });
+
+        var responseBody = body.data
+        var events = responseBody.entries
+
+        totalEvents = totalEvents.concat(events)
+
+        let nextLinkBody = responseBody.links.find(link => link.relation === 'next');
+        if(nextLinkBody === undefined)
+          getAllEventUrl = undefined
+        else
+          getAllEventUrl = nextLinkBody.uri+"?embed=body";
+
+      }
+      console.log("start calculate")
+
+      var sum = 0;
+      var size = totalEvents.length;
+      for(var i = size-1 ; i >=0 ; i--){
+        let date = new Date(totalEvents[i].updated)
+        if(date < requestDateTime) {
+          const eventData = JSON.parse(totalEvents[i].data)
+          sum += eventData.amount
+        }
+        else {
+          break;
+        }
+
+      }
+      res.send({
+        totalBalance: sum
+      });
     }
-
     else {
       request(getProjectionUrl,
           function (error, response, body) {
