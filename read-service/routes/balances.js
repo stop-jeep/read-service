8a16dd12242fe8bcdc2f85e376033a0b2bb7f2a4
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2019-08-16 13:35:01 +0700

    complete endpoint to get user balance

--- read-service/routes/balances.js
+++ read-service/routes/balances.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:userId/balances', function(req, res, next) {
-  res.send(req.params.userId);
+router.get('/:userId/balances', function (req, res, next) {
+    var userId = req.params.userId;
+    var getProjectionUrl = 'http://34.87.47.20:2113/projection/user_{userId}_projection/state';
+    getProjectionUrl = getProjectionUrl.replace('{userId}',userId);
+
+    console.log(getProjectionUrl);
+
+    request(getProjectionUrl,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                res.send(body);
+            } else {
+                res.send('user not found');
+            }
+        })
 });
 
 module.exports = router;
